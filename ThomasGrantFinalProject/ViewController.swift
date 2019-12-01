5bea4a210e6209befaca11df3ab2e55e212d5c43
Grant Thomas <gmthomas@usc.edu>
2019-12-01 13:40:20 -0800

    Initial Commit

---
+++ ThomasGrantFinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThomasGrantFinalProject
+//
+//  Created by Grant Thomas on 12/1/19.
+//  Copyright © 2019 Grant Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
